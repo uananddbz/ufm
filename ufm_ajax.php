cec7763d90f51915e207cee8e0c0899776b9878a
utkarsh anand <uananddbz@gmail.com>
2014-03-16 16:40:36 +0530

    creating
    php file manger using ufm plugin

---
+++ ufm_ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['base'])) {
+
+include('ufm_class.php');
+
+$base=$_POST['base'];
+$fm=new ufm($base);
+
+
+$url=$_POST['url'];
+
+$files=$fm->show_files($url);
+$links=$fm->breadcrumb($url);
+echo json_encode(array('files'=>$files,'links'=>$links));
+}
+
+?>
